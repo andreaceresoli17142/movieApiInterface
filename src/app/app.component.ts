1c9472ae5508e0fd482d36dd31419504ae6188ab
andreaceresoli17142 <56073866+andreaceresoli17142@users.noreply.github.com>
2021-09-28 08:06:06 +0200

    finished pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,12 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
 
   //TODO: css
-  //TODO: mettere qualcosa che descriva il film quando non c'e' l'immagine
-  //TODO: dare la possibilita' di vedere pure le altre pagine dell'api
   //TODO: (forse) aggiungiere titolo, autore e anno di rilascio sotto i poster
 
-  //! BUG: se il programma non riceve un intera pagina non mette le locandine nella tabella
 
   title = 'compitoTps';
 
@@ -37,20 +35,81 @@ export class AppComponent {
 
     this.page = 1;
     this.apiReq( true );
-
   }
 
-  apiReq( first = false ): void{
-    const url = "https://api.themoviedb.org/3/search/movie?api_key=f8f299c7a98e07f7962dacfe6f8ee16f&language=en-US&query=" + this.movieDataGroup.value.movieTitle + "&page=" + this.page; // Replace it with your own API path
+  apiReq( first = false, page = 1, bypassLocalStorage = false ): void{
+
+    if ( bypassLocalStorage == false ){
+
+      var localData = this.getFromStorage(  this.movieDataGroup.value.movieTitle, page );
+
+      if ( localData != null ){
+
+        console.log("from local storage");
+        this.processData( localData, first );
+        return;
+      }
+    }
+
+    const url = "https://api.themoviedb.org/3/search/movie?api_key=f8f299c7a98e07f7962dacfe6f8ee16f&language=en-US&query=" + this.movieDataGroup.value.movieTitle + "&page=" + page; // Replace it with your own API path
     this.http.get(url).subscribe(data => {
-      console.log(data);
-      let temp: any = data;
+      console.log("from api");
+
+      this.saveInStorage( this.movieDataGroup.value.movieTitle , page, data );
+
+      this.processData( data, first );
+    });
+  }
+
+  processData( data: any, first = false ):void  {
+    let temp: any = data;
       let tempData: any = temp.results;
       
       if ( first )
         this.maxPage = temp.total_pages;
       this.apiResponse = this.arrayToMatrix(tempData );
+  }
+
+
+
+  saveInStorage( query: string, page: number, data: any ): void{
+    localStorage.setItem( query.toUpperCase() + "-" + page, JSON.stringify(data));
+  }
+
+  getFromStorage( query: string, page: number ): any{
+
+    var localData = localStorage.getItem(query.toUpperCase() + "-" + page);
+
+    if (localData == null)
+      return null;
+
+    return JSON.parse( localData );
+  }
+  
+  apiCallSuccess(data: any): void {
+    
+    this.movieDataGroup.patchValue({
+      movieTitle: ''
     });
+  }
+  
+  openModal( movieData: any ): void {
+    const modalRef = this.modalService.open(OnApiResponseComponent,{
+      scrollable: true,
+      windowClass: 'myCustomModalClass',
+      // keyboard: false,
+      backdrop: 'static'
+    });
+    
+    modalRef.componentInstance.fromParent = movieData;
+    modalRef.result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+  onPageChange( page: number ): void {
+    // console.log("pagination element respondend: "+page);
+    this.apiReq( false, page );
   }
 
   arrayToMatrix( data: any ): any {
@@ -74,25 +133,4 @@ export class AppComponent {
     return tempMatrix;
   }
 
-  apiCallSuccess(data: any) {
-
-    this.movieDataGroup.patchValue({
-      movieTitle: ''
-    });
-  }
-
-  openModal( movieData: any ) {
-    const modalRef = this.modalService.open(OnApiResponseComponent,
-      {
-        scrollable: true,
-        windowClass: 'myCustomModalClass',
-        // keyboard: false,
-        backdrop: 'static'
-      });
-
-    modalRef.componentInstance.fromParent = movieData;
-    modalRef.result.then((result) => {
-    }, (reason) => {
-    });
-  }
 }
